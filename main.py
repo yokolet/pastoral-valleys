f964d9c413e04ce506ea8945699e3eda400e574f
YOKO HARADA <yokolet@gmail.com>
2017-01-19 00:11:54 -0500

    feat: Adds very simple map view
    This commit is still a proof of concept. Very simple map view
    has been added and is working.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import json
 import logging
 from flask import Flask, render_template, request
 
+
 app = Flask(__name__)
+
+MAP_CONFIG = json.loads(
+    open('app-config.json', 'r').read())['map']
 
 @app.route('/')
 def hello():
@@ -10,6 +15,14 @@ def hello():
 @app.route('/cat-clicker')
 def catClicker():
     return render_template('cat-clicker.html')
+
+@app.route('/marker-sample')
+def markedMap():
+    callback = 'initMap'
+    url = MAP_CONFIG['url']
+    url = url + 'key='+ MAP_CONFIG['api-key']
+    url = url + '&callback=' + callback
+    return render_template('marker-sample.html', url=url)
 
 @app.errorhandler(500)
 def server_error(e):
