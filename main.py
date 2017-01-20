ce23c1ca8b7c27d2de076714237d6cd251709c62
YOKO HARADA <yokolet@gmail.com>
2017-01-19 20:59:15 -0500

    feat: Adds very simple map by knockout and api sample
    Still not an actual app, but to test how map will show up
    using knockout, the code is added. Also, another test for api
    has been added. Both are working with some odds. This will be
    fixed later when the app becomes actual one.

--- main.py
+++ main.py
@@ -24,6 +24,21 @@ def markedMap():
     url = url + '&callback=' + callback
     return render_template('marker-sample.html', url=url)
 
+@app.route('/soda-sample')
+def sodaSample():
+    callback = 'initSodaMap'
+    url = MAP_CONFIG['url']
+    url = url + 'key='+ MAP_CONFIG['api-key']
+    url = url + '&callback=' + callback
+    return render_template('soda-sample.html', url=url)
+
+@app.route('/ko-marker-sample')
+def koMarkedMap():
+    callback = 'initMap'
+    url = MAP_CONFIG['url']
+    url = url + 'key='+ MAP_CONFIG['api-key']
+    return render_template('ko-marker-sample.html', url=url)
+
 @app.errorhandler(500)
 def server_error(e):
     logging.exception('An error occurred during a request.')
--- static/js/app.js
+++ static/js/app.js
@@ -87,4 +87,65 @@ var ViewModel = function() {
     };
 }
 
-ko.applyBindings(new ViewModel());
+//ko.applyBindings(new ViewModel());
+
+ko.bindingHandlers.map = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var mapObj = ko.utils.unwrapObservable(valueAccessor());
+        var latLng = new google.maps.LatLng(
+            ko.utils.unwrapObservable(mapObj.lat),
+            ko.utils.unwrapObservable(mapObj.lng));
+        var mapOptions = { center: latLng,
+                           zoom: 6,
+                           mapTypeId: google.maps.MapTypeId.ROADMAP};
+
+        mapObj.googleMap = new google.maps.Map(element, mapOptions);
+
+        mapObj.marker = new google.maps.Marker({
+            map: mapObj.googleMap,
+            position: latLng,
+            title: "You Are Here",
+            draggable: true
+        });
+
+        var infoObj = new google.maps.InfoWindow({
+            content: '<b>Here we are!</b>'
+        });
+
+        mapObj.marker.addListener('click', function() {
+            infoObj.open(mapObj.googleMap, mapObj.marker);
+        });
+
+        mapObj.onChangedCoord = function(newValue) {
+            var latLng = new google.maps.LatLng(
+                ko.utils.unwrapObservable(mapObj.lat),
+                ko.utils.unwrapObservable(mapObj.lng));
+            mapObj.googleMap.setCenter(latLng);
+        };
+
+        mapObj.onMarkerMoved = function(dragEnd) {
+            var latLng = mapObj.marker.getPosition();
+            mapObj.lat(latLng.lat());
+            mapObj.lng(latLng.lng());
+        };
+
+        mapObj.lat.subscribe(mapObj.onChangedCoord);
+        mapObj.lng.subscribe(mapObj.onChangedCoord);
+
+        google.maps.event.addListener(mapObj.marker, 'dragend', mapObj.onMarkerMoved);
+
+        $("#" + element.getAttribute("id")).data("mapObj",mapObj);
+    }
+};
+
+var myMapViewModel = function() {
+    var self = this;
+    self.myMap = ko.observable({
+        lat: ko.observable(40.76),
+        lng: ko.observable(-73.98)
+    });
+}
+
+$(document).ready(function () {
+   ko.applyBindings(new myMapViewModel);
+});
