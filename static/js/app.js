20f225526369993f2de04d1a73c90c9acc67cd80
YOKO HARADA <yokolet@gmail.com>
2017-01-21 18:39:10 -0500

    feat: As cursor moves, data will be updated
    A center cursor is draggable. When a cursor moves, a new data set
    will be loaded and updated view will show up.

--- static/js/app.js
+++ static/js/app.js
@@ -15,37 +15,10 @@ ko.bindingHandlers.map = {
             title: "Center",
             draggable: true
         });
-
-        url = "/api/JSON?lat=" +
-            ko.utils.unwrapObservable(mapObj.lat) +
-            "&lng=" +
-            ko.utils.unwrapObservable(mapObj.lng);
-        // TODO: center is draggable
-        // but, other marker is not
-        // when center is dragged, it should call api
-        $.getJSON(url, function(data) {
-            console.log("data should be here")
-            $.each(data, function(i, entry) {
-                if (entry.position) {
-                    var marker = new google.maps.Marker({
-                        map: mapObj.googleMap,
-                        position: new google.maps.LatLng(entry.position.lat,
-                                                         entry.position.lng),
-                        title: entry.title,
-                        icon: entry.icon,
-                    });
-                    var infoObj = new google.maps.InfoWindow({
-                        content: entry.content
-                    });
-                    marker.addListener('click', function() {
-                        infoObj.open(mapObj.googleMap, marker);
-                    });
-                    google.maps.event.addListener(marker, 'dragend', mapObj.onMarkerMoved);
-                }
-            });
-        });
+        updateData(mapObj);
 
         mapObj.onChangedCoord = function(newValue) {
+            console.log('onchangecoord');
             var latLng = new google.maps.LatLng(
                 ko.utils.unwrapObservable(mapObj.lat),
                 ko.utils.unwrapObservable(mapObj.lng));
@@ -53,37 +26,12 @@ ko.bindingHandlers.map = {
         };
 
         mapObj.onMarkerMoved = function(dragEnd) {
+            console.log('onmarkmoved');
             var latLng = mapObj.marker.getPosition();
             mapObj.lat(latLng.lat());
             mapObj.lng(latLng.lng());
-            url = "/api/JSON?lat=" +
-                latLng.lat() +
-                "&lng=" +
-                latLng.lng();
-            // TODO: center is draggable
-            // but, other marker is not
-            // when center is dragged, it should call api
-            $.getJSON(url, function(data) {
-                console.log("data should be here")
-                $.each(data, function(i, entry) {
-                    if (entry.position) {
-                        var marker = new google.maps.Marker({
-                            map: mapObj.googleMap,
-                            position: new google.maps.LatLng(entry.position.lat,
-                                                             entry.position.lng),
-                            title: entry.title,
-                            icon: entry.icon,
-                        });
-                        var infoObj = new google.maps.InfoWindow({
-                            content: entry.content
-                        });
-                        marker.addListener('click', function() {
-                            infoObj.open(mapObj.googleMap, marker);
-                        });
-                        google.maps.event.addListener(marker, 'dragend', mapObj.onMarkerMoved);
-                    }
-                });
-            });
+            console.log('on fn: ' + mapObj.markers().length);
+            updateData(mapObj);
         };
 
         mapObj.lat.subscribe(mapObj.onChangedCoord);
@@ -94,11 +42,47 @@ ko.bindingHandlers.map = {
     }
 };
 
+var updateData = function(mapObj) {
+    console.log('in updatedata: ' + mapObj.markers().length);
+    console.log('0 ' + mapObj.markers()[0]);
+    for(i=0; i<mapObj.markers().length; i++) {
+         m = mapObj.markers().shift();
+         m.setMap(null);
+    }
+    console.log('after shifted: ' + mapObj.markers().length);
+    url = "/api/JSON?lat=" +
+        ko.utils.unwrapObservable(mapObj.lat) +
+        "&lng=" +
+        ko.utils.unwrapObservable(mapObj.lng);
+    $.getJSON(url, function(data) {
+        console.log("data should be here")
+        $.each(data, function(i, entry) {
+            if (entry.position) {
+                var marker = new google.maps.Marker({
+                    map: mapObj.googleMap,
+                    position: new google.maps.LatLng(entry.position.lat,
+                                                     entry.position.lng),
+                    title: entry.title,
+                    icon: entry.icon,
+                });
+                var infoObj = new google.maps.InfoWindow({
+                    content: entry.content
+                });
+                marker.addListener('click', function() {
+                    infoObj.open(mapObj.googleMap, marker);
+                });
+                mapObj.markers().push(marker);
+            }
+        });
+    });
+};
+
 var myMapViewModel = function() {
     var self = this;
     self.myMap = ko.observable({
         lat: ko.observable(35.8572),
-        lng: ko.observable(-78.7147)
+        lng: ko.observable(-78.7147),
+        markers: ko.observableArray()
     });
 }
 
