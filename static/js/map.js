98e0538e51807c74b5a6d6469d1961c06b054a0b
YOKO HARADA <yokolet@gmail.com>
2017-01-29 00:12:35 -0500

    chore: Chooses balanced places and JavaScript comments
    This commit is a sort of finishing up. Comments were added to
    JavaScript. Locations were changed so that markers exists in
    balanced positions.

--- static/js/map.js
+++ static/js/map.js
@@ -1,3 +1,7 @@
+/**
+* Location object definition
+* @param {Object} data - location data
+*/
 var Location = function(data) {
     this.areaname = data["name"];
     this.lat = data["lat"];
@@ -7,29 +11,35 @@ var Location = function(data) {
 // Knockout JS custom handler definition
 ko.bindingHandlers.anothermap = {
     init: function (element, valueAccessor, allBindings, viewModel) {
+        // values come from html data-binding
         var value = valueAccessor();
         var areaname = value.areaname;
         var lat = value.centerLat;
         var lng = value.centerLng;
         console.log("init: " + areaname + ", "+ lat + ", " + lng);
         var mapObj = ko.unwrap(value.mapState);
-        mapObj.current(new Location({"areaname": areaname, "lat": lat, "lng": lng}));
+        // Set initial center position
+        mapObj.initialLoc(new Location({"areaname": areaname, "lat": lat, "lng": lng}));
+        // Load all area name data
         areanameData(mapObj);
         $("#" + element.getAttribute("id")).data("mapObj",mapObj);
     },
     update: function (element, valueAccessor, allBindings, viewModel) {
         var value = valueAccessor();
         var mapObj = ko.unwrap(value.mapState);
-        var latLng = new google.maps.LatLng(value.centerLat,
-                                            value.centerLng);
+        console.log("update:" + mapObj.initialLoc().lat + ", " + value.centerLat);
+        var latLng = new google.maps.LatLng(mapObj.initialLoc().lat,
+                                            mapObj.initialLoc().lng);
         var mapOptions = { center: latLng,
-                           zoom: 10, 
+                           zoom: 12,
                            mapTypeId: google.maps.MapTypeId.ROADMAP};
+        // Render a map
         mapObj.googleMap = new google.maps.Map(element, mapOptions);
 
+        // Render location markers
         mapObj.onAreanameLoad = function(data) {
             console.log("onAreanameLoad");
-            console.log(mapObj.current());
+            console.log(mapObj.initialLoc());
             mapObj.locations().forEach(function(loc) {
                 console.log("onAreanameLoad: " + loc.lat + ", " + loc.lng);
                 var content_string = "Area Name: <b>" + loc.areaname + "</b>";
@@ -44,18 +54,22 @@ ko.bindingHandlers.anothermap = {
                         content: content_string
                     })
                 });
+                // Load crime data when a location marker is clicked,
                 marker.addListener("click", function() {
                     updateData(mapObj, marker);
                 });
-                // add listener function when infowindow is closed.
+                // Add listener function when infowindow is closed.
                 marker.info.addListener("closeclick", function() {
                     marker.setAnimation(null);
                 });
+                // Save a marker
                 mapObj.locMarkers().push(marker);
             });
         }
+        // Watches location data loading completion
         mapObj.locationLoaded.subscribe(mapObj.onAreanameLoad);
 
+        // Deselect old location marker when another location is clicked
         mapObj.onCurrentChange = function(data) {
             console.log("current: " + mapObj.currentMarker().title);
             var previous = mapObj.previousMarker();
@@ -66,10 +80,12 @@ ko.bindingHandlers.anothermap = {
             var marker = mapObj.currentMarker();
             marker.info.open(mapObj.googleMap, marker);
             marker.setAnimation(google.maps.Animation.BOUNCE);
+            mapObj.googleMap.setCenter(marker.position);
         }
+        // Watches a currentMarker state change
         mapObj.currentMarker.subscribe(mapObj.onCurrentChange);
 
-        // a callback function when the filter is changed
+        // Re-render crime markers looking at a current category state
         mapObj.onChangedFilter = function(data) {
             var size = mapObj.crimeMarkers().length;
             var markers = mapObj.crimeMarkers();
@@ -82,10 +98,12 @@ ko.bindingHandlers.anothermap = {
                 }
             }
         }
+        // Watches a category(filter) state change
         mapObj.category.subscribe(mapObj.onChangedFilter);
     }
 };
 
+// Create DOM elements to show error message
 var error_message = function(text) {
     var message = '';
     message += '<div class="callout alert" data-closable>\n';
@@ -97,6 +115,7 @@ var error_message = function(text) {
     return $(message)
 }
 
+// Load location data
 var areanameData = function(mapObj) {
     url = "/api/location/JSON?";
     $.getJSON(url, function(data) {
@@ -105,8 +124,8 @@ var areanameData = function(mapObj) {
             var loc = new Location(entry);
             mapObj.locations().push(loc);
         });
-        console.log(mapObj.current().lat);
-        console.log(mapObj.current().lng);
+        console.log(mapObj.initialLoc().lat);
+        console.log(mapObj.initialLoc().lng);
         mapObj.locationLoaded(true);
     })
         .fail(function(jqxhr, textStatus, error) {
@@ -115,6 +134,7 @@ var areanameData = function(mapObj) {
         });
 };
 
+// Checks whether the crime marker should be visible or not
 var isVisible = function(mapObj, entry) {
     if ("all" == mapObj.category()) {
         return true;
@@ -125,8 +145,9 @@ var isVisible = function(mapObj, entry) {
     }
 };
 
+// Load crime data
 var updateData = function(mapObj, marker) {
-    // clear all markers in the old place
+    // Clear all markers in the old place
     while(mapObj.crimeMarkers().length > 0) {
         m = mapObj.crimeMarkers().shift()
         m.setMap(null);
@@ -134,12 +155,12 @@ var updateData = function(mapObj, marker) {
     var lat = marker.position.lat();
     var lng = marker.position.lng();
     console.log("updateData: " + lat + ", " + lng);
-    // make a request to API endpoint
+    // Make a request to API endpoint
     url = "/api/JSON?lat=" + lat + "&lng=" + lng;
     $.getJSON(url, function(data) {
         $.each(data, function(i, entry) {
             if (entry.position) {
-                // create each marker
+                // Create each marker
                 var marker = new google.maps.Marker({
                     map: mapObj.googleMap,
                     position: new google.maps.LatLng(entry.position.lat,
@@ -149,20 +170,20 @@ var updateData = function(mapObj, marker) {
                     title: entry.title,
                     icon: entry.icon,
                 });
-                // create an infowindow of this marker
+                // Create an infowindow of this marker
                 var infoObj = new google.maps.InfoWindow({
                     content: entry.content
                 });
-                // add lister function when infowindow is opened.
+                // Add lister function when infowindow is opened.
                 marker.addListener("click", function() {
                     infoObj.open(mapObj.googleMap, marker);
                     marker.setAnimation(google.maps.Animation.BOUNCE);
                 });
-                // add listener function when infowindow is closed.
+                // Add listener function when infowindow is closed.
                 infoObj.addListener("closeclick", function() {
                     marker.setAnimation(null);
                 });
-                // saves markers
+                // Save a marker
                 mapObj.crimeMarkers().push(marker);
             }
         });
@@ -182,23 +203,32 @@ var updateData = function(mapObj, marker) {
 var mapModel = function() {
     var self = this;
     self.mapState = ko.observable({
+        // location (area) data
         locations: ko.observableArray([]),
+        // flag to signal location data loading completion
         locationLoaded: ko.observable(false),
-        current: ko.observable(),
+        // initial center location
+        initialLoc: ko.observable(),
+        // all location markers
         locMarkers: ko.observableArray([]),
+        // previous location marker
         previousMarker: ko.observable(),
+        // current location marker
         currentMarker: ko.observable(),
+        // all crime markers of the current location
         crimeMarkers: ko.observableArray([]),
+        // current category (filter)
         category: ko.observable("all"),
     });
 
+    // Change a current location
     this.setCurrent = function(index, lat, lng) {
         console.log(index + ", " + lat + ", " + lng);
         self.mapState().previousMarker(self.mapState().currentMarker());
         self.mapState().currentMarker(self.mapState().locMarkers()[parseInt(index)-1]);
     };
 
-    // a function called when filter button is clicked
+    // Change a current category (filter)
     this.setCategory = function(data) {
         self.mapState().category(data);
     };
